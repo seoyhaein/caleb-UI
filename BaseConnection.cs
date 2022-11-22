785c8d9a9539a29fd44dbbf9f2cde9a2a2248768
Young Jun Seo <fighton.yj@gmail.com>
2022-11-22 16:09:00 +0900

    geometry 추가적인 study 필요.

---
+++ BaseConnection.cs
@@ -0,0 +1,68 @@
+﻿using Avalonia.Controls.Shapes;
+using Avalonia;
+using Avalonia.Media;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace caleb_UI
+{
+    // https://docs.avaloniaui.net/misc/wpf/uielement-frameworkelement-and-control
+    // http://reference.avaloniaui.net/api/Avalonia/Vector/
+    // http://reference.avaloniaui.net/api/Avalonia.Media/StreamGeometry/
+
+
+    public abstract class BaseConnection : Shape
+    {
+        /// <summary>
+        /// Gets a vector that has its coordinates set to 0.
+        /// </summary>
+        protected static readonly Vector ZeroVector = new Vector(0d, 0d);
+
+        // TODO 상세하게 살펴보자.
+        private readonly StreamGeometry _geometry = new StreamGeometry
+        {
+            //FillRule = FillRule.EvenOdd
+        };
+
+        // geometry 설정하기.
+
+        protected override Geometry CreateDefiningGeometry()
+        {
+            using (StreamGeometryContext context = _geometry.Open())
+            {
+                (Vector sourceOffset, Vector targetOffset) = GetOffset();
+                Point source = Source + sourceOffset;
+                Point target = Target + targetOffset;
+
+                (Point arrowSource, Point arrowTarget) = DrawLineGeometry(context, source, target);
+
+                if (ArrowSize.Width != 0d && ArrowSize.Height != 0d)
+                {
+                    switch (ArrowEnds)
+                    {
+                        case ArrowHeadEnds.Start:
+                            DrawArrowGeometry(context, arrowTarget, arrowSource, ConnectionDirection.Backward);
+                            break;
+                        case ArrowHeadEnds.End:
+                            DrawArrowGeometry(context, arrowSource, arrowTarget, ConnectionDirection.Forward);
+                            break;
+                        case ArrowHeadEnds.Both:
+                            DrawArrowGeometry(context, arrowSource, arrowTarget, ConnectionDirection.Forward);
+                            DrawArrowGeometry(context, arrowTarget, arrowSource, ConnectionDirection.Backward);
+                            break;
+                        case ArrowHeadEnds.None:
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+
+            return _geometry;
+        }
+    }
+}
