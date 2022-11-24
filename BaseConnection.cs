36a365a6597bbeb21a8a96c437df8137d9fd5c76
Young Jun Seo <fighton.yj@gmail.com>
2022-11-24 19:49:30 +0900

    1차 수정

--- BaseConnection.cs
+++ BaseConnection.cs
@@ -1,7 +1,10 @@
 ﻿using Avalonia.Controls.Shapes;
 using Avalonia;
 using Avalonia.Media;
+using Avalonia.Input;
+
 using System;
+using System.Windows.Input;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -54,9 +57,142 @@ namespace caleb_UI
         Edge,
     }
 
+    /// <summary>
+    /// The direction in which a connection is oriented.
+    /// </summary>
+    public enum ConnectionDirection
+    {
+        /// <summary>
+        /// From <see cref="BaseConnection.Source"/> to <see cref="BaseConnection.Target"/>.
+        /// </summary>
+        Forward,
+
+        /// <summary>
+        /// From <see cref="BaseConnection.Target"/> to <see cref="BaseConnection.Source"/>.
+        /// </summary>
+        Backward
+    }
+
+    /// <summary>
+    /// The end at which the arrow head is drawn.
+    /// </summary>
+    public enum ArrowHeadEnds
+    {
+        /// <summary>
+        /// Arrow head at start.
+        /// </summary>
+        Start,
+
+        /// <summary>
+        /// Arrow head at end.
+        /// </summary>
+        End,
+
+        /// <summary>
+        /// Arrow heads at both ends.
+        /// </summary>
+        Both,
+
+        /// <summary>
+        /// No arrow head.
+        /// </summary>
+        None
+    }
+
 
     public abstract class BaseConnection : Shape
-    {
+    { 
+        #region Dependency Properties
+        // 일단, StyledProperty 로 진행하고 추후에 DirectProperty 를 살펴보자.
+
+        //public static readonly DependencyProperty SourceProperty = DependencyProperty.Register(nameof(Source), typeof(Point), typeof(BaseConnection), new FrameworkPropertyMetadata(BoxValue.Point, FrameworkPropertyMetadataOptions.AffectsRender));
+        //public static readonly DependencyProperty TargetProperty = DependencyProperty.Register(nameof(Target), typeof(Point), typeof(BaseConnection), new FrameworkPropertyMetadata(BoxValue.Point, FrameworkPropertyMetadataOptions.AffectsRender));
+        //public static readonly DependencyProperty SourceOffsetProperty = DependencyProperty.Register(nameof(SourceOffset), typeof(Size), typeof(BaseConnection), new FrameworkPropertyMetadata(BoxValue.Size, FrameworkPropertyMetadataOptions.AffectsRender));
+        //public static readonly DependencyProperty TargetOffsetProperty = DependencyProperty.Register(nameof(TargetOffset), typeof(Size), typeof(BaseConnection), new FrameworkPropertyMetadata(BoxValue.Size, FrameworkPropertyMetadataOptions.AffectsRender));
+        //public static readonly DependencyProperty OffsetModeProperty = DependencyProperty.Register(nameof(OffsetMode), typeof(ConnectionOffsetMode), typeof(BaseConnection), new FrameworkPropertyMetadata(default(ConnectionOffsetMode), FrameworkPropertyMetadataOptions.AffectsRender));
+        //public static readonly DependencyProperty DirectionProperty = DependencyProperty.Register(nameof(Direction), typeof(ConnectionDirection), typeof(BaseConnection), new FrameworkPropertyMetadata(default(ConnectionDirection), FrameworkPropertyMetadataOptions.AffectsRender));
+        //public static readonly DependencyProperty ArrowHeadEndsProperty = DependencyProperty.Register(nameof(ArrowEnds), typeof(ArrowHeadEnds), typeof(BaseConnection), new FrameworkPropertyMetadata(ArrowHeadEnds.End, FrameworkPropertyMetadataOptions.AffectsRender));
+        //public static readonly DependencyProperty SpacingProperty = DependencyProperty.Register(nameof(Spacing), typeof(double), typeof(BaseConnection), new FrameworkPropertyMetadata(BoxValue.Double0, FrameworkPropertyMetadataOptions.AffectsRender));
+        //public static readonly DependencyProperty ArrowSizeProperty = DependencyProperty.Register(nameof(ArrowSize), typeof(Size), typeof(BaseConnection), new FrameworkPropertyMetadata(BoxValue.ArrowSize, FrameworkPropertyMetadataOptions.AffectsRender));
+        
+        // 아래 두개는 일단 확인하자.
+        //public static readonly DependencyProperty SplitCommandProperty = DependencyProperty.Register(nameof(SplitCommand), typeof(ICommand), typeof(BaseConnection));
+        //public static readonly DependencyProperty DisconnectCommandProperty = Connector.DisconnectCommandProperty.AddOwner(typeof(BaseConnection));
+
+
+        /// <summary>
+        /// Defines the <see cref="SourceProperty"/> property.
+        /// </summary>
+        public static readonly StyledProperty<Point> SourceProperty =
+            AvaloniaProperty.Register<BaseConnection, Point>(nameof(Source));
+
+        /// <summary>
+        /// Defines the <see cref="TargetProperty"/> property.
+        /// </summary>
+        public static readonly StyledProperty<Point> TargetProperty =
+            AvaloniaProperty.Register<BaseConnection, Point>(nameof(Target));
+
+
+        /// <summary>
+        /// Defines the <see cref="SourceOffsetProperty"/> property.
+        /// </summary>
+        public static readonly StyledProperty<Size> SourceOffsetProperty =
+            AvaloniaProperty.Register<BaseConnection, Size>(nameof(SourceOffset));
+
+        /// <summary>
+        /// Defines the <see cref="TargetOffsetProperty"/> property.
+        /// </summary>
+        public static readonly StyledProperty<Size> TargetOffsetProperty =
+            AvaloniaProperty.Register<BaseConnection, Size>(nameof(TargetOffset));
+
+        /// <summary>
+        /// Defines the <see cref="OffsetModeProperty"/> property.
+        /// </summary>
+        public static readonly StyledProperty<ConnectionOffsetMode> OffsetModeProperty =
+            AvaloniaProperty.Register<BaseConnection, ConnectionOffsetMode>(nameof(SourceOffset));
+
+        /// <summary>
+        /// Defines the <see cref="DirectionProperty"/> property.
+        /// </summary>
+        public static readonly StyledProperty<ConnectionDirection> DirectionProperty =
+            AvaloniaProperty.Register<BaseConnection, ConnectionDirection>(nameof(Direction));
+
+        /// <summary>
+        /// Defines the <see cref="ArrowHeadEndsProperty"/> property.
+        /// </summary>
+        public static readonly StyledProperty<ArrowHeadEnds> ArrowHeadEndsProperty =
+            AvaloniaProperty.Register<BaseConnection, ArrowHeadEnds>(nameof(ArrowEnds));
+
+        /// <summary>
+        /// Defines the <see cref="SpacingProperty"/> property.
+        /// </summary>
+        public static readonly StyledProperty<double> SpacingProperty =
+            AvaloniaProperty.Register<BaseConnection, double>(nameof(Spacing));
+
+        /// <summary>
+        /// Defines the <see cref="ArrowSizeProperty"/> property.
+        /// </summary>
+        public static readonly StyledProperty<Size> ArrowSizeProperty =
+            AvaloniaProperty.Register<BaseConnection, Size>(nameof(ArrowSize));
+
+        // avaloniaUI button 참고 하자.
+        // ex
+        /*
+        /// <summary>
+        /// Defines the <see cref="Command"/> property.
+        /// </summary>
+        public static readonly DirectProperty<Button, ICommand?> CommandProperty =
+            AvaloniaProperty.RegisterDirect<Button, ICommand?>(nameof(Command),
+                button => button.Command, (button, command) => button.Command = command, enableDataValidation: true);
+         
+         */
+
+        /// <summary>
+        /// Defines the <see cref="SplitCommandProperty"/> property.
+        /// </summary>
+       /* public static readonly StyledProperty<ICommand> SplitCommandProperty =
+            AvaloniaProperty.Register<BaseConnection, ICommand>(nameof(SplitCommand));*/
+
         /// <summary>
         /// Gets or sets the start point of this connection.
         /// </summary>
@@ -75,6 +211,24 @@ namespace caleb_UI
             set => SetValue(TargetProperty, value);
         }
 
+        /// <summary>
+        /// Gets or sets the offset from the <see cref="Target"/> point.
+        /// </summary>
+        public Size TargetOffset
+        {
+            get => (Size)GetValue(TargetOffsetProperty);
+            set => SetValue(TargetOffsetProperty, value);
+        }
+
+        /// <summary>
+        /// Gets or sets the offset from the <see cref="Source"/> point.
+        /// </summary>
+        public Size SourceOffset
+        {
+            get => (Size)GetValue(SourceOffsetProperty);
+            set => SetValue(SourceOffsetProperty, value);
+        }
+
 
         /// <summary>
         /// Gets or sets the <see cref="ConnectionOffsetMode"/> to apply when drawing the connection.
@@ -84,6 +238,55 @@ namespace caleb_UI
             get => (ConnectionOffsetMode)GetValue(OffsetModeProperty);
             set => SetValue(OffsetModeProperty, value);
         }
+
+
+        /// <summary>
+        /// Gets or sets the direction in which this connection is oriented.
+        /// </summary>
+        public ConnectionDirection Direction
+        {
+            get => (ConnectionDirection)GetValue(DirectionProperty);
+            set => SetValue(DirectionProperty, value);
+        }
+
+        /// <summary>
+        /// Gets or sets the arrow ends.
+        /// </summary>
+        public ArrowHeadEnds ArrowEnds
+        {
+            get => (ArrowHeadEnds)GetValue(ArrowHeadEndsProperty);
+            set => SetValue(ArrowHeadEndsProperty, value);
+        }
+
+        /// <summary>
+        /// Gets or sets the size of the arrow head.
+        /// </summary>
+        public Size ArrowSize
+        {
+            get => (Size)GetValue(ArrowSizeProperty);
+            set => SetValue(ArrowSizeProperty, value);
+        }
+
+        /// <summary>
+        /// The distance between the start point and the where the angle breaks.
+        /// </summary>
+        public double Spacing
+        {
+            get => (double)GetValue(SpacingProperty);
+            set => SetValue(SpacingProperty, value);
+        }
+
+        /// <summary>
+        /// Splits the connection. Triggered by <see cref="EditorGestures.Connection.Split"/> gesture.
+        /// Parameter is the location where the splitting ocurred.
+        /// </summary>
+      /*  public ICommand? SplitCommand
+        {
+            get => (ICommand)GetValue(SplitCommandProperty);
+            set => SetValue(SplitCommandProperty, value);
+        }*/
+
+        #endregion
 
         /// <summary>
         /// Gets a vector that has its coordinates set to 0.
@@ -150,6 +353,118 @@ namespace caleb_UI
                 ConnectionOffsetMode.None => (ZeroVector, ZeroVector),
                 _ => throw new ArgumentOutOfRangeException()
             };
+        }
+
+        // connection 에서 가져옴.
+        //protected abstract (Point ArrowSource, Point ArrowTarget) DrawLineGeometry(StreamGeometryContext context, Point source, Point target);
+
+
+        // ReSharper disable once InconsistentNaming
+        private const double _baseOffset = 100d;
+        // ReSharper disable once InconsistentNaming
+        private const double _offsetGrowthRate = 25d;
+
+        protected (Point ArrowSource, Point ArrowTarget) DrawLineGeometry(StreamGeometryContext context, Point source, Point target)
+        {
+            double direction = Direction == ConnectionDirection.Forward ? 1d : -1d;
+            var spacing = new Vector(Spacing * direction, 0d);
+            var arrowOffset = new Vector(ArrowSize.Width * direction, 0d);
+            Point endPoint = Spacing > 0 ? target - arrowOffset : target;
+            Point startPoint = source + spacing;
+
+            Vector delta = target - source;
+            double height = Math.Abs(delta.Y);
+            double width = Math.Abs(delta.X);
+
+            // Smooth curve when distance is lower than base offset
+            double smooth = Math.Min(_baseOffset, height);
+            // Calculate offset based on distance
+            double offset = Math.Max(smooth, width / 2d);
+            // Grow slowly with distance
+            offset = Math.Min(_baseOffset + Math.Sqrt(width * _offsetGrowthRate), offset);
+
+            var controlPoint = new Vector(offset * direction, 0d);
+
+            // TODO 세밀한 부분은 찾아봐야 한다.
+            context.BeginFigure(source, false);
+            context.LineTo(startPoint);
+            // TODO QuadraticBezierTo(Point, Point) 랑 비교해보자.
+            context.CubicBezierTo(startPoint + controlPoint, endPoint - controlPoint, endPoint);
+            context.LineTo(endPoint);
+
+            return (source, target);
+        }
+
+        protected virtual void DrawArrowGeometry(StreamGeometryContext context, Point source, Point target, ConnectionDirection arrowDirection = ConnectionDirection.Forward)
+        {
+            (Point from, Point to) = GetArrowHeadPoints(source, target, arrowDirection);
+
+            
+            // avalonia 에서 찾아보자. 위에랑 비교하고 nodify 랑 비교해야함.
+            context.BeginFigure(target, true);
+            context.LineTo(from);
+            context.LineTo(to);
+        }
+
+        protected virtual (Point From, Point To) GetArrowHeadPoints(Point source, Point target, ConnectionDirection arrowDirection)
+        {
+            double headWidth = ArrowSize.Width;
+            double headHeight = ArrowSize.Height;
+
+            double direction = arrowDirection == ConnectionDirection.Forward ? 1d : -1d;
+            var from = new Point(target.X - headWidth * direction, target.Y + headHeight);
+            var to = new Point(target.X - headWidth * direction, target.Y - headHeight);
+            return (from, to);
+        }
+
+        private static Vector GetRectangleModeOffset(Vector delta, Size offset)
+        {
+            if (delta.SquaredLength > 0d)
+            {
+                delta.Normalize();
+            }
+
+            double angle = Math.Atan2(delta.Y, delta.X);
+
+            // 수정 구문 살펴보자.
+            // 참고
+            // https://sourcegraph.com/github.com/mameolan/Avalonia.ExtendedToolkit/-/blob/Avalonia.Controlz/Controls/TabPanel.cs?L166:20&subtree=true#tab=references
+            var result = new Vector();
+
+            if (offset.Width * 2d * Math.Abs(delta.Y) < offset.Height * 2d * Math.Abs(delta.X))
+            {
+                double X = Math.Sign(delta.X) * offset.Width;
+                result = new Vector(X, Math.Tan(angle) * X);
+                
+            }
+            else
+            {
+                double Y = Math.Sign(delta.Y) * offset.Height;
+                double X = 1.0d / Math.Tan(angle) * Y;
+
+                result = new Vector(X, Y);
+              
+            }
+
+            return result;
+        }
+
+        private static Vector GetCircleModeOffset(Vector delta, Size offset)
+        {
+            if (delta.SquaredLength > 0d)
+            {
+                delta.Normalize();
+            }
+
+            return new Vector(delta.X * offset.Width, delta.Y * offset.Height);
+        }
+
+        private static Vector GetEdgeModeOffset(Vector delta, Size offset)
+        {
+            double xOffset = Math.Min(Math.Abs(delta.X) / 2d, offset.Width) * Math.Sign(delta.X);
+            double yOffset = Math.Min(Math.Abs(delta.Y) / 2d, offset.Height) * Math.Sign(delta.Y);
+
+            return new Vector(xOffset, yOffset);
         }
     }
 }
